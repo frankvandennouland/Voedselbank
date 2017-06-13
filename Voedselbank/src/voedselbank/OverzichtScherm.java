af6dc202d344655f7eb680f19f76ca517ce3d529
Dave <Dave@Dave-PC.wireless.hhs.nl>
2017-06-13 23:38:23 +0200

    Client per hulpverlener

--- Voedselbank/src/voedselbank/OverzichtScherm.java
+++ Voedselbank/src/voedselbank/OverzichtScherm.java
@@ -49,13 +49,10 @@ public class OverzichtScherm extends javax.swing.JFrame {
 
         overzichtTabel.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+
             }
         ));
         jScrollPane1.setViewportView(overzichtTabel);
